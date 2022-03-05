ab6a687df00f0ac1e924182a685f7dcd633dd87b
Raze91 <hattab_sami@yahoo.fr>
2022-03-05 22:21:59 +0100

    add new data and images

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("zbeub");
+const options = {
+    // section qui contient l'élément
+    root: document.querySelector('#scrollArea'),
+    // hitbox de detection de l'élément
+    rootMargin: '0px',
+    // visibilité de l'élément
+    threshold: 1.0
+};
+
+const observer = new IntersectionObserver(() => {
+
+}, options);

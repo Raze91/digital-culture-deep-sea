093cbcd11dd19d03ecda3faa00099bfd30fa82ce
Raze91 <hattab_sami@yahoo.fr>
2022-03-10 17:40:36 +0100

    stop the submarine at the bottom

--- index.js
+++ index.js
@@ -4,12 +4,31 @@ const options = {
     // hitbox de detection de l'élément
     rootMargin: '0px',
     // visibilité de l'élément
-    threshold: 1
+    threshold: 1.0
 };
 
-const observer = new IntersectionObserver((entries, observer) => {
-    console.log(observer)
-    // document.querySelector(".submarine").classList.replace("submarine", "fixed-submarine");
+const endOptions = {
+    // section qui contient l'élément
+    root: document.querySelector('#scrollArea'),
+    // hitbox de detection de l'élément
+    rootMargin: '0px',
+    // visibilité de l'élément
+    threshold: 0
+}
+
+const observer = new IntersectionObserver((entries) => {
+    console.log(entries)
+    if (entries[0].isIntersecting) {
+        document.querySelector(".submarine").classList.replace("submarine", "fixed-submarine");
+    }
 }, options);
 
+const endObserver = new IntersectionObserver((entries) => {
+    if (entries[0].isIntersecting) {
+        console.log("end");
+        document.querySelector(".fixed-submarine").classList.replace("fixed-submarine", "bottom-submarine");
+    }
+}, endOptions)
+
 observer.observe(document.querySelector('.submarine'))
+endObserver.observe(document.querySelector(".conclusion"))
